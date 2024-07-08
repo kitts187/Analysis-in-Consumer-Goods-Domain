612fed73b41d3ca844b75aebd58582d31a2b9c55
Pranjal Sharma <141250584+kitts187@users.noreply.github.com>
2024-07-09 03:05:30 +0530

    Create Request 3.sql

---
+++ SQL Codes/Request 3.sql
@@ -0,0 +1,6 @@
+/*3. Provide a report with all the unique product counts for each  segment  and 
+sort them in descending order of product counts. The final output contains 
+2 fields, 
+segment 
+product_count*/
+
